c10d0c35b1b22e7298998cb61aa8eece5d41d1f7
AliAlkhtri <234427638+alialkhtri3-png@users.noreply.github.com>
2026-07-22 20:49:45 +0300

    Sovereign Identity Rust Core V132 Update

--- rust/wallet_core_rs/src/ffi/identity/mod.rs
+++ rust/wallet_core_rs/src/ffi/identity/mod.rs
@@ -1830,3 +1830,4 @@ r#"{{
 // Sovereign Identity Rust Core V129 Release
 // Sovereign Identity Rust Core V130 Release
 // Sovereign Identity Rust Core V131 Release
+// Sovereign Identity Rust Core V132 Release

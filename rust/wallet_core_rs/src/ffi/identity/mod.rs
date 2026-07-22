883e8a34eb7a3096041d8095e7b44083a20bb8d7
AliAlkhtri <234427638+alialkhtri3-png@users.noreply.github.com>
2026-07-22 20:47:52 +0300

    Sovereign Identity Rust Core V86 Update

--- rust/wallet_core_rs/src/ffi/identity/mod.rs
+++ rust/wallet_core_rs/src/ffi/identity/mod.rs
@@ -1684,3 +1684,4 @@ r#"{{
 // Sovereign Identity Rust Core V83 Release
 // Sovereign Identity Rust Core V84 Release
 // Sovereign Identity Rust Core V85 Release
+// Sovereign Identity Rust Core V86 Release

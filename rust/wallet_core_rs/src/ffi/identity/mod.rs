69824ada1e46849034864bd1b2e08b60dedc9fd5
AliAlkhtri <234427638+alialkhtri3-png@users.noreply.github.com>
2026-07-22 20:49:40 +0300

    Sovereign Identity Rust Core V100 Update

--- rust/wallet_core_rs/src/ffi/identity/mod.rs
+++ rust/wallet_core_rs/src/ffi/identity/mod.rs
@@ -1798,3 +1798,4 @@ r#"{{
 // Sovereign Identity Rust Core V97 Release
 // Sovereign Identity Rust Core V98 Release
 // Sovereign Identity Rust Core V99 Release
+// Sovereign Identity Rust Core V100 Release
